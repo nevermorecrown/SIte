800a49eabef2618ccdfbfdfccd595fe11d51f716
nevermorecrown <44512882+nevermorecrown@users.noreply.github.com>
2020-05-10 23:11:21 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let btn = document.getElementById("inform");
+let infoContainer = -1;
+
+btn.addEventListener("click", function () {
+
+    let ourRequest = new XMLHttpRequest();
+
+    ourRequest.open('GET', 'games.json');
+    ourRequest.onload = function() {
+        if (ourRequest.status >= 200 && ourRequest.status < 400) {
+            let ourData = JSON.parse(ourRequest.responseText);
+            btn.insertAdjacentHTML('beforebegin', ourData[infoContainer].content);
+        } else {
+            console.log("We connected to the server, but it returned an error.");
+        }
+    }
+
+    ourRequest.onerror = function() {
+        console.log("Connection error");
+    };
+
+    ourRequest.send();
+    infoContainer++;
+    if (infoContainer >= 1) {
+        btn.classList.add("hide-me");
+    }
+});
